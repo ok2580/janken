004aaa64fc459451e542a75a6a663f5a025048ee
ok2258 <gomo20031119@gmail.com>
2024-10-26 13:44:23 +0900

    ユーザ情報をselectして表示

--- src/main/java/oit/is/z2303/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2303/kaizi/janken/controller/JankenController.java
@@ -1,6 +1,8 @@
 package oit.is.z2303.kaizi.janken.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,19 +12,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z2303.kaizi.janken.model.Entry;
+import oit.is.z2303.kaizi.janken.model.User;
+//import oit.is.z2303.kaizi.janken.model.MatchMapper;
+//import oit.is.z2303.kaizi.janken.model.Match;
+import oit.is.z2303.kaizi.janken.model.UserMapper;
 
 @Controller
 public class JankenController {
 
+  //@Autowired
+  //private Entry room;
   @Autowired
-  private Entry room;
+  UserMapper userMapper;
+  //@Autowired
+  //MatchMapper matchMapper
 
   @GetMapping("/janken")
   public String step1(Principal prin, ModelMap model) {
     String loginUser = prin.getName();
-    this.room.addUser(loginUser);
-    model.addAttribute("room", this.room);
-
+    //this.room.addUser(loginUser);
+    model.addAttribute("loginUser", loginUser);
+    ArrayList<User> users1 = userMapper.selectAll();
+    model.addAttribute("users1", users1);
     return "janken.html";
   }
 
---
+++ src/main/java/oit/is/z2303/kaizi/janken/model/UserMapper.java
@@ -0,0 +1,19 @@
+package oit.is.z2303.kaizi.janken.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+  @Insert("INSERT INTO users (userName) VALUES (#{userName});")
+  @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
+  void insertUser(User users);
+
+
+  @Select("SELECT * from users")
+  ArrayList<User> selectAll();
+}

1cbca842fc8e1a2eee9f2b5c35901ed27da89e69
ok2258 <gomo20031119@gmail.com>
2024-10-26 10:19:16 +0900

    完成

--- src/main/java/oit/is/z2303/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2303/kaizi/janken/controller/JankenController.java
@@ -1,21 +1,28 @@
 package oit.is.z2303.kaizi.janken.controller;
 
+import java.security.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import oit.is.z2303.kaizi.janken.model.Entry;
 
 @Controller
 public class JankenController {
-  @PostMapping("/janken")
-  public String janken(@RequestParam String player, ModelMap model) {
-    model.addAttribute("player", player);
-    return "janken.html";
-  }
+
+  @Autowired
+  private Entry room;
 
   @GetMapping("/janken")
-  public String janken() {
+  public String step1(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+
     return "janken.html";
   }
 
@@ -34,8 +41,6 @@ public class JankenController {
     }
     model.addAttribute("Result", Result);
     model.addAttribute("hand", hand);
-    // ModelMap型変数のmodelにtasuResult2という名前の変数で，tasuResultの値を登録する．
-    // ここで値を登録するとthymeleafが受け取り，htmlで処理することができるようになる
     return "janken.html";
 
   }
